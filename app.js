d78adf7eb9399645a9f6f8172cb5dc78c2c0c7ea
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-12-06 21:42:09 +0545

    added the post method

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+app.use(express.json());
 const tours = JSON.parse(fs.readFileSync("./tours.json"));
-app.get("/api/v1/tours", (req, res) => {
-	res.status(200).json({
-		state: "success",
-		length: tours.length,
-		data: {
-			tours,
-		},
-	});
+// app.get("/api/v1/tours", (req, res) => {
+// 	res.status(200).json({
+// 		state: "success",
+// 		length: tours.length,
+// 		data: {
+// 			tours,
+// 		},
+// 	});
+// });
+app.post("/api/v1/tours", (req, res) => {
+	console.log(req.body);
+	res.send("Done");
 });
 app.listen(8001, () => {
 	console.log("__Listening To Port 8001__");
