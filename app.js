b83157b0a9b95b7e5beaa92d3e79889743d31af8
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-12-09 06:45:52 +0545

    added the delete request

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 app.use(express.json());
-const tours = JSON.parse(fs.readFileSync("./tours-simple.json"));
+const tours = JSON.parse(fs.readFileSync("./data/tours-simple.json"));
 app.get("/api/v1/tours", (req, res) => {
+	console.log(tours);
 	res.status(200).json({
 		state: "success",
 		length: tours.length,
@@ -12,18 +13,59 @@ app.get("/api/v1/tours", (req, res) => {
 		},
 	});
 });
+app.get("/api/v1/tours/:userId", (req, res) => {
+	const id = req.params.userId * 1;
+	const searchedTours = tours.find((item) => item.id === id);
+	if (!searchedTours) {
+		return res.status(400).json({
+			status: "Error",
+			body: "Error 404 ",
+		});
+	}
+	res.status(200).json({
+		status: "Success",
+		body: searchedTours,
+	});
+});
+app.patch("/api/v1/tours/:id", (req, res) => {
+	const id = req.params.id;
+	console.log(req.body);
+	if (id > tours.length)
+		return res.status(400).json({
+			status: "failed",
+			message: "Error 404",
+		});
+	res.status(200).json({
+		status: "success",
+		data: {
+			message: "This is successful",
+		},
+	});
+});
 app.post("/api/v1/tours", (req, res) => {
 	const newId = tours[tours.length - 1]._id + 1;
 	const newTour = Object.assign({ id: newId }, req.body);
 	console.log(tours.length);
 	tours.push(newTour);
-	fs.writeFile("./tours-simple.json", JSON.stringify(tours), (err) => {
+	fs.writeFile("./data/tours-simple.json", JSON.stringify(tours), (err) => {
 		res.status(201).json({
 			status: "success",
 			data: {
 				tours: newTour,
 			},
 		});
+	});
+});
+app.delete("/api/v1/tours/:id", (req, res) => {
+	const id = req.params.id;
+	if (id > tours.length) {
+		return res.status(400).json({
+			status: "failed",
+			message: "Error",
+		});
+	}
+	res.status(204).json({
+		data: null,
 	});
 });
 app.listen(8001, () => {
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const server = require("http").createServer();
 server.on("request", (req, res) => {
     //request type 1
-	// fs.readFile("./tours.json", (err, data) => {
+	// fs.readFile("./data/tours.json", (err, data) => {
 	// 	if (err) console.error(err.message);
 	// 	res.end(data)
 	// });
     //request type 2
     //^ response not able to read as fast as the reading process is done !
-    // const readable = fs.createReadStream("./test-file.txt")
+    // const readable = fs.createReadStream("./data/test-file.txt")
     // readable.on('data',chunk=>{
     //     res.write(chunk)
     // })
@@ -22,7 +22,7 @@ server.on("request", (req, res) => {
     // })
     //With pipe operator we can handle the 'data' , 'end' and 'error' very easily
     //^we are now going to use the pipe operator
-    const readable = fs.createReadStream('./test-file.txt')
+    const readable = fs.createReadStream('./data/test-file.txt')
     readable.pipe(res)
 });
 server.listen(8001,'127.0.0.1',()=>{
