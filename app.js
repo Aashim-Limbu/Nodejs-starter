abd992f6cfae8ad3079c5afaa8875126428e33f5
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-12-20 21:38:56 +0545

    configure serverfile.js

--- app.js
+++ app.js
@@ -6,6 +6,4 @@ app.use(express.json());
 
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/tours", toursRouter);
-app.listen(8001, "127.0.0.1", () => {
-	console.log("__Listening to port 8001__");
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require("./app")
+app.listen(8001,'127.0.0.1',()=>{
+console.log("__LIstening to the port 8001__")
+})
