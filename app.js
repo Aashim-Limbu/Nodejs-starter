2dc1929452d9da90822f030cce7acb5e34e5e95a
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-12-21 23:46:44 +0545

    serving the static file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const toursRouter = require("./routes/toursRouter");
 const usersRouter = require("./routes/usersRouter");
 const app = express();
 app.use(express.json());
+app.use(express.static("./public"))
 
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/tours", toursRouter);
